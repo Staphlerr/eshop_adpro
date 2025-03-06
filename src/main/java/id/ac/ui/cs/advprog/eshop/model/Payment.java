24e90fe731f6b2049ea7e4070df611f1540029d9
Staphlerr <belvaghaniabhinaya2020@gmail.com>
2025-03-07 01:31:26 +0700

    [GREEN] Implement Payment Model Class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Builder;
 import lombok.Getter;
 import java.util.Map;
+import java.util.Arrays;
 
 @Getter
 @Builder
@@ -14,8 +15,26 @@ public class Payment {
     private Order order;
 
     public Payment(String id, String method, String status, Map<String, String> paymentData, Order order) {
+        if (order == null) {
+            throw new IllegalArgumentException("Order cannot be null.");
+        }
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.order = order;
+        setStatus(status);
     }
 
     public void setStatus(String status) {
+        String[] validStatuses = {"PENDING", "SUCCESS", "REJECTED"};
+        if (Arrays.stream(validStatuses).noneMatch(valid -> valid.equals(status))) {
+            throw new IllegalArgumentException("Invalid payment status: " + status);
+        }
+        this.status = status;
+        if (status.equals("SUCCESS")) {
+            this.order.setStatus("SUCCESS");
+        } else if (status.equals("REJECTED")) {
+            this.order.setStatus("FAILED");
+        }
     }
 }
